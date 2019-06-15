835e8eb4752fcf64d48ce023e5c5f1bb13169572
Jenny Rakete <alan.dalisdadavis@gmail.com>
2019-06-15 12:25:16 +0200

    Commit Message

---
+++ entry_template.php
@@ -0,0 +1,9 @@
+<?php
+
+$entry = "<div class=\"entry\">
+    <img alt=\"Foto von $name_german\" src=\"$plant_img\" class=\"plant_img\"/>
+    <span class=\"name_german\">$name_german</span>
+    <br>
+    <span class=\"name_latin\">$name_latin</span>
+    <p class=\"short_description\">$short_description</p>
+</div>";
