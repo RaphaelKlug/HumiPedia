0406295f61705a82cefd6cf362abe1b022b57e90
Gucky <51849622+RaphaelKlug@users.noreply.github.com>
2021-04-06 18:03:20 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,21 @@
 <?php
+function scanAll(){
 $scan = scandir('data');
-foreach($scan as $file) {
-     if (!is_dir("data/$file")) {
-     echo $file.'\n';    }
-} 
+$result= [];
+foreach($scan as $dir) {
+     if (is_dir("data/$dir")) {
+     $result[]=$dir;    }
+}
+print_r($result);
+return $result;
+}
+function searchq($query){
+     $arr=scanAll();
+     $key=array_search($query,$arr);
+     if($key==false){return "<div><h1>Nothing found</h1></div>";}
+     $name=$arr[$key];
+     $data=file_get_contents("data/$name/daten.txt");
+     $icon="data/$name/icon.jpg";
+     return '<div><h1>'.$name.'</h1><p><img src="'.$icon.'" alt="'.$name.'"></p><p>'.$data.'</p></div>';
+}
 ?>
