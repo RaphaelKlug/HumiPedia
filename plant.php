3f046d2cd6f5ee9323b789eb76c84df37e7bc11a
Gucky <51849622+RaphaelKlug@users.noreply.github.com>
2021-04-06 21:24:01 +0200

    Update plant.php

--- plant.php
+++ plant.php
@@ -3,9 +3,17 @@
         $plant_id = $_GET['plant_id'];
         require("db.php");
 	$result=searchq($plant_id);
-    }
 	$plant_name=substr($result, strpos($result, "<h1>")+4, strpos($result, "<h1>")-strpos($result, "<h1>"-4));
-?>
+    if(isset($_GET['AHH'])){
+	header('Content-Type: application/json');
+	$JsonData=array();
+	$JsonData->name=$plant_name;
+	header('Content-Length: '.strlen(json_encode($JSONData)));
+	echo json_encode($JsonData);
+}else{}
+}
+
+	?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
